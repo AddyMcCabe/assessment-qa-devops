3a302dacb5628a0dba16d3ef058629af1f46c888
Addison McCabe <addisonhartmccabe@gmail.com>
2022-05-20 10:42:21 -0700

    initial commit

---
+++ functions.test.ts
@@ -0,0 +1,11 @@
+const {shuffleArray} = require('./utils')
+
+describe('shuffleArray should', () => {
+    // test("check to see if items in shuffleArray are the same as returned array ", () => {
+    //     expect(shuffleArray()[i]).toBe(shuffleArray[i]);
+    //   })
+
+      test("check to see if returned array has the same length as shuffleArray", () => {
+        expect(shuffleArray().length).toEqual(shuffleArray.length);
+      })
+})
